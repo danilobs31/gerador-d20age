0203bb1c9676f471525458b6eb29fe857f33c221
Danilo Borges <daniloborgessilva31@gmail.com>
2023-07-25 15:26:37 -0300

    alterações na parte visual

--- javascript.js
+++ javascript.js
@@ -414,11 +414,11 @@ const itens = [
 
 const armaduras = [
     { nome: 'Sem armadura', preco: 0, bonus: 0, nota: 'Sem armadura', peso: 0, CA: 9 },
-    { nome: 'Couro simples', preco: 10, bonus: 1, nota: 'couro simples de animais comuns', peso: 100, CA: 8 },
-    { nome: 'Couro fervido', preco: 20, bonus: 2, nota: 'couro feito de forma especializada', peso: 200, CA: 7 },
-    { nome: 'Lamelar', preco: 40, bonus: 3, nota: 'couro fervido e lâminas de metal', peso: 300, CA: 6 },
-    { nome: 'Anelar', preco: 80, bonus: 4, nota: 'couro fervido e anéis de metal', peso: 400, CA: 5 },
-    { nome: 'Segmentada', preco: 100, bonus: 5, nota: 'couro fervido e placas de metal', peso: 500, CA: 4 },
+    { nome: 'Armadura de Couro simples', preco: 10, bonus: 1, nota: 'couro simples de animais comuns', peso: 100, CA: 8 },
+    { nome: 'Armadura de Couro fervido', preco: 20, bonus: 2, nota: 'couro feito de forma especializada', peso: 200, CA: 7 },
+    { nome: 'Armadura Lamelar', preco: 40, bonus: 3, nota: 'couro fervido e lâminas de metal', peso: 300, CA: 6 },
+    { nome: 'Armadura Anelar', preco: 80, bonus: 4, nota: 'couro fervido e anéis de metal', peso: 400, CA: 5 },
+    { nome: 'Armadura Segmentada', preco: 100, bonus: 5, nota: 'couro fervido e placas de metal', peso: 500, CA: 4 },
     { nome: 'Couraça', preco: 200, bonus: 6, nota: 'couro fervido, anelar e placas de metal', peso: 600, CA: 3 },
     { nome: 'Escudo', preco: 10, bonus: 1, nota: 'Feitos de madeira ou metal', peso: 100, CA: 1 },
 ];
@@ -631,6 +631,13 @@ if (escudo != null) {
     bolsa.push(escudo);
 }
 
+let listaItens = "";
+
+for (let i = 0; i < bolsa.length; i++) {
+    listaitens = '${bolsa[i].nome} [${bolsa[i].peso} Cn]'
+};
 
 let classeArmadura = `${ca1}(${ca2})`;
+
+console.log(listaItens);
 
